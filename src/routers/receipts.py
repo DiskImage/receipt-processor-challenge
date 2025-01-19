3f2467fd4d0a8704e1ad8f47688c31ee6648ee9c
Ian Graves <ian@iangrav.es>
2025-01-19 16:36:05 -0600

    Refactors receipts router to consume the receipt_processing_service

--- src/routers/receipts.py
+++ src/routers/receipts.py
@@ -5,12 +5,14 @@ from pydantic import Field
 from src.models.points_response import PointsResponse
 from src.models.process_receipt_response import ProcessReceiptResponse
 from src.models.receipt import Receipt
+from src.services.receipt_processing_service import ReceiptNotFound, ReceiptProcessingService
 
 router = APIRouter(prefix="/receipts")
 
 BAD_REQUEST_MESSAGE = "The receipt is invalid."
 NOT_FOUND_MESSAGE = "No receipt found for that ID."
 
+processing_service = ReceiptProcessingService()
 
 @router.post(
     path="/process",
@@ -22,7 +24,8 @@ NOT_FOUND_MESSAGE = "No receipt found for that ID."
     },
 )
 def process_receipt(request: Request, receipt: Receipt):
-    return Response(status_code=status.HTTP_200_OK)
+    new_receipt_id = processing_service.process_receipt(receipt=receipt)
+    return ProcessReceiptResponse(id=new_receipt_id)
 
 
 @router.get(
@@ -37,4 +40,8 @@ def process_receipt(request: Request, receipt: Receipt):
 def get_receipt_points(
     id: Annotated[str, Field(pattern="^\\S+$", description="The ID of the receipt.")]
 ):
-    return Response(status_code=status.HTTP_200_OK)
+    try:
+      points_found = processing_service.get_points_for_receipt(id=id)
+    except ReceiptNotFound:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=NOT_FOUND_MESSAGE)
+    return PointsResponse(points=points_found)
